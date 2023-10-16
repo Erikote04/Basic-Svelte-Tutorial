500cabdfdde54150abc288478c9a8c2084703073
Erikote <erikote2004@icloud.com>
2023-10-17 00:03:10 +0200

    Refactoring the project. Now each [Subject] has his own files

---
+++ Svelte Tutorial/src/Props.svelte
@@ -0,0 +1,18 @@
+<script>
+// @ts-nocheck
+  import Nested from './Nested-For-Props.svelte';
+  import PackageInfo from './PackageInfo.svelte';
+	const pkg = {
+		name: 'svelte',
+		speed: 'blazing',
+		version: 4,
+		website: 'https://svelte.dev'
+	};
+</script>
+
+<main>
+  <Nested answer={42} />
+  <Nested />
+  <PackageInfo {...pkg}/>
+</main>
+
---
+++ Svelte Tutorial/src/Reactivity.svelte
@@ -0,0 +1,38 @@
+<script>
+  let count = 0;
+  function increment() {
+    count += 1;
+  }
+  $: doubled = count * 2;
+  $: if (count >= 10) {
+    alert('count is dangerously high!');
+    count = 0;
+  }
+  let numbers = [1, 2, 3, 4];
+  function addNumber() {
+    numbers[numbers.length] = numbers.length + 1;
+  }
+  $: sum = numbers.reduce((total, currentNumber) => total + currentNumber, 0);
+</script>
+
+<main>
+  <button on:click={increment}>
+    Clicked {count}
+    {count === 1 ? 'time' : 'times'}
+  </button>
+
+  <p>{count} doubled is {doubled}</p>
+
+  <p>{numbers.join(' + ')} = {sum}</p>
+  <button on:click={addNumber}>
+    Add a number
+  </button>
+</main>
+
+<style>
+  p {
+		color: goldenrod;
+		font-family: 'Comic Sans MS', cursive;
+		font-size: 2em;
+	}
+</style>
