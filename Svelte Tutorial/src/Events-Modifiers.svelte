a9698f2d5d93e10983a1c3246ffbf2732b3c1f9d
Erikote <erikote2004@icloud.com>
2023-10-22 17:35:42 +0200

    [Events] Event modifiers

---
+++ Svelte Tutorial/src/Events-Modifiers.svelte
@@ -0,0 +1,8 @@
+<button on:click={() => alert('clicked')}>
+	Click me
+</button>
+
+<!--
+    This is a handler event at it's natural state.
+    Every time we click the button an alert will appear.
+-->
